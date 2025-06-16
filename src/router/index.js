e6a3b4844f4239094aad9aace65aaf28ab854534
Ulises Aimar <uaimar75@gmail.com>
2025-06-16 16:17:23 -0300

    Propiedades computadas. v-show y v-if

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,13 @@ const router = createRouter({
       name: 'registrar',
       
       component: () => import('../modules/registro/views/RegistrarView.vue'),
-    }
+    },
+    {
+      path: '/calcular',
+      name: 'calcular',
+      
+      component: () => import('../modules/calcular/views/CalcularView.vue'),
+    },
   ],
 })
 
